1d56173a6fd27f915de33a6212af9ffd75170e68
John Sangwon Suh <johnsuh94@gmail.com>
2020-05-26 10:44:14 -0400

    fix typo with var declaration

--- include/webcachesim/filters/bloom.h
+++ include/webcachesim/filters/bloom.h
@@ -177,8 +177,8 @@ class KHitCounter {
 public:
     uint64_t second_hit_byte, unevicted_kth_hit_byte, evicted_kth_hit_byte;
     KHitCounter(const std::map <std::string, std::string> &params) {
-        CountingSetFilter _filter();
-        filter = &filter;
+        CountingSetFilter _filter;
+        filter = &_filter;
         filter->init_with_params(params);
     }
 
