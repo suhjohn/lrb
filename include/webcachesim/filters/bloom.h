f0303f12ba6a48549a37471b2d73ef1c1dcce003
John Sangwon Suh <johnsuh94@gmail.com>
2020-06-11 11:40:48 -0400

    add set filter

--- include/webcachesim/filters/bloom.h
+++ include/webcachesim/filters/bloom.h
@@ -21,61 +21,51 @@ using bsoncxx::builder::basic::sub_array;
 
 using namespace std;
 
-//class SetFilter : public Filter {
-//public:
-//    SetFilter() : Filter() {}
-//
-//    bool should_filter(SimpleRequest &req) {
-//        return !exist_or_insert(req.get_id());
-//    }
-//
-//    inline bool exist(uint64_t key) {
-//        return (_filters[0].find(key)) != _filters[0].end() || (_filters[1].find(key) != _filters[1].end());
-//    }
-//
-//    inline bool exist_or_insert(uint64_t key) {
-//        if (exist(key))
-//            return true;
-//        else
-//            insert(key);
-//        return false;
-//    }
-//
-//    inline void insert(uint64_t key) {
-//        if (n_added_obj > max_n_element) {
-//            _filters[1 - current_filter].clear();
-//            current_filter = 1 - current_filter;
-//            n_added_obj = 0;
-//        }
-//        _filters[current_filter].insert(key);
-//        ++n_added_obj;
-//    }
-//
-//    void init_with_params(const std::map <std::string, std::string> &params) override {
-//        for (auto &it: params) {
-//            if (it.first == "max_n_element") {
-//                std::istringstream iss(it.second);
-//                size_t new_n;
-//                iss >> new_n;
-//                max_n_element = new_n;
-//            } else {
-//                cerr << "Set filter unrecognized parameter: " << it.first << endl;
-//            }
-//        }
-//        cerr << "Init Set filter. max_n_element: " << max_n_element;
-//        unordered_set <uint64_t> filter_1;
-//        unordered_set <uint64_t> filter_2;
-//        _filters[0] = filter_1;
-//        _filters[1] = filter_2;
-//    }
-//
-//    size_t max_n_element = 40000000;
-//    uint8_t current_filter = 0;
-//    int n_added_obj = 0;
-//    unordered_set <uint64_t> _filters[2];
-//};
-//
-//static FilterFactory <SetFilter> factorySetFilter("Set");
+class SetFilter : public Filter {
+public:
+    SetFilter() : Filter() {}
+
+    void init_with_params(const std::map <std::string, std::string> &params) override {
+        for (auto &it: params) {
+            if (it.first == "max_n_element") {
+                std::istringstream iss(it.second);
+                size_t new_n;
+                iss >> new_n;
+                max_n_element = new_n;
+            } else if (it.first == "filter_k") {
+                k = stoi(it.second);
+            } else {
+                cerr << "Set filter unrecognized parameter: " << it.first << endl;
+            }
+        }
+        cerr << "Init Set filter. max_n_element: " << max_n_element << " fp_rate: " << fp_rate << " k: " << k << endl;
+        for (int i = 0; i < k; i++) {
+            unordered_set <uint64_t> filter;
+            filters.push_back(filter);
+        }
+    }
+
+    size_t total_bytes_used() override {
+        return 0;
+    }
+
+    void update_stat(bsoncxx::v_noabi::builder::basic::document &doc) override {
+        doc.append(kvp("filter_size", std::to_string(total_bytes_used())));
+        doc.append(kvp("max_n_element", std::to_string(max_n_element)));
+        doc.append(kvp("filter_k", k));
+    }
+
+    bool should_filter(SimpleRequest &req) override;
+
+    size_t max_n_element = 40000000;
+    uint16_t curr_filter_idx = 0;
+    int n_added_obj = 0;
+    int k = 2;
+
+    std::vector<unordered_set <uint64_t>> filters;
+};
+
+static FilterFactory <SetFilter> factorySetFilter("Set");
 
 class BloomFilter : public Filter {
 public:
--- src/filters/bloom.cpp
+++ src/filters/bloom.cpp
@@ -24,6 +24,26 @@ bool BloomFilter::should_filter(SimpleRequest &req) {
 }
 
 
+bool SetFilter::should_filter(SimpleRequest &req) {
+    auto key = req.get_id();
+    for (int i = 0; i < k; i++) {
+        if (filters[i].find(key) != filters[i].end()) {
+            return false;
+        }
+    }
+    if (n_added_obj > max_n_element) {
+        // clear the least recently used filter, which will now be our current filter
+        curr_filter_idx = (curr_filter_idx + 1) % k;
+        filters[curr_filter_idx]->clear();
+        n_added_obj = 0;
+    }
+    filters[curr_filter_idx].insert(key);
+    // keep track of number of requests for the current filter
+    ++n_added_obj;
+    return true;
+}
+
+
 bool CountingSetFilter::should_filter(SimpleRequest &req) {
     auto key = req.get_id();
     for (int i = 0; i < k; i++) {
