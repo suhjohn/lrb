fb7a45fa6f40b03ecbe11d852398c19cd3846bc8
John Sangwon Suh <johnsuh94@gmail.com>
2020-06-16 15:03:48 -0400

    set segment window

--- include/webcachesim/filters/bloom.h
+++ include/webcachesim/filters/bloom.h
@@ -428,11 +428,12 @@ public:
 
     AccessResourceCounter(
             const string &trace_file, uint n_extra_fields,
-            int64_t n_early_stop = -1, uint64_t segment_window = 1000000,
+            int64_t n_early_stop = -1, uint64_t _segment_window = 1000000,
             int _bucket_count = 4) {
         cerr << "init AccessResourceCounter" << endl;
         bucket_count = _bucket_count;
-
+        segment_window = _segment_window;
+        
         for (int i = 0; i < bucket_count; i++) {
             current_buckets.push_back(0);
         }
