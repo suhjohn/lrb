233749f114900bada78f970812daf0bad2ccaa75
John Sangwon Suh <johnsuh94@gmail.com>
2020-06-15 22:27:44 -0400

    fix wrong counter incrementing seq

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -337,8 +337,8 @@ bsoncxx::builder::basic::document FrameWork::simulate() {
         }
 
         ++seq;
-        if (track_access_frequency_hit) {
-            accessFrequencyCounter->incr_seq();
+        if (track_access_resource_hit) {
+            accessResourceCounter->incr_seq();
         }
     }
     delete req;
